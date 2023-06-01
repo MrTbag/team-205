4750926d8d76dc292027f4a894d4f559fc67d0dd
Danial Gharib <danial.mail.gh@gmail.com>
2023-06-01 10:47:39 +0330

    6

--- predictor.v
+++ predictor.v
@@ -1,44 +1,41 @@
 module predictor(input wire request, result, clk, taken, output reg prediction);
 
-reg [1:0] state = 2'b11;
+reg [1:0] state = 2'b00;
 always @(posedge clk) begin
-	if (result)
-	begin
-	switch(state):
-	case 2'b00:
-	begin
-	if (taken)
-	state <= 2'b01;
-	else
-	state <= 2'b00;
-	end
-	case 2'b01:
-	begin
-	if (taken)
-	state <= 2'b10;
-	else
-	state <= 2'b00;
-	end
-	case 2'b10:
-	begin
-	if (taken)
-	state <= 2'b11;
-	else
-	state <= 2'b01;
-	case 2'b11:
-	begin
-	if (taken)
-	state <= 2'b11;
-	else 
-	state <= 2'b10;
-	end
-	default: state <= 2'b11;
-end
-	if (request)
-	begin
-	prediction <= state[1];
-	end
-	
-end		
+  if (result)
+  begin
+    if (state == 2'b00)
+    begin
+      if (taken)
+      state <= 2'b01;
+      else
+      state <= 2'b00;
+    end
+    if (state == 2'b01)
+    begin
+      if (taken)
+      state <= 2'b10;
+      else
+      state <= 2'b00;
+    end
+    if (state == 2'b10)
+    begin
+      if (taken)
+      state <= 2'b11;
+      else
+      state <= 2'b01;
+    end
+    if (state == 2'b11)
+    begin
+      if (taken)
+      state <= 2'b11;
+      else 
+      state <= 2'b10;
+    end
+  end
+  if (request)
+    prediction <= state[1];
+  
+end    
 
 endmodule
